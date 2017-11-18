6192a66fdaceb46113c457efb3b1efc8b681b337
ezigag <31944165+ezigag@users.noreply.github.com>
2017-11-18 16:30:48 +0700

    Addpet

--- PetHotel/src/Customer/Customer.java
+++ PetHotel/src/Customer/Customer.java
@@ -5,6 +5,7 @@
  */
 package Customer;
 
+import LinkDB.CustomerPetDB;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -18,23 +19,25 @@ import javax.persistence.Query;
 @Entity
 public class Customer {
        
-        private int PrimaryKey;        //Primary Key
-	private String Name;
-        private String Tel;
-        private String Email;
-        private String IdCardNumber;
-        private String Plan;
+        private int primaryKey;        //Primary Key
+	private String name;
+        private String tel;
+        private String email;
+        private String idcardNumber;
+        private String plan;
         
         protected float cost = 0;   //cost
 
-    public Customer(int PrimaryKey, String Name, String Tel, String Email, String IdCardNumber, String Plan) {
-        this.PrimaryKey = PrimaryKey;
-        this.Name = Name;
-        this.Tel = Tel;
-        this.Email = Email;
-        this.IdCardNumber = IdCardNumber;
-        this.Plan = Plan;
-    } //Constructor
+    public Customer(int primaryKey, String name, String tel, String email, String idcardNumber, String plan) {
+        this.primaryKey = primaryKey;
+        this.name = name;
+        this.tel = tel;
+        this.email = email;
+        this.idcardNumber = idcardNumber;
+        this.plan = plan;
+    }
+
+    
     
         public void setCost(float cost) {
             this.cost = cost;
@@ -49,10 +52,21 @@ public class Customer {
         }  // Calculate realcost(cost - discount)
         
         public void AddPet(String pet_ID){
-            ////////DATA BASE//////
+           
+        CustomerPetDB obj; //Customer object
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("$dist/db/CustomerPet.odb");		
+	EntityManager em = emf.createEntityManager();
+        
+        em.getTransaction().begin();
+        
+        obj = new CustomerPetDB(this.primaryKey,pet_ID);   //Add username + Pet ID
+	em.persist(obj);
+
+
+	em.getTransaction().commit();
         }
         
-        public void addCustomer(String Name, String Tel, String Email, String IdCardNumber, String Plan){
+        public void addCustomer(String name, String tel, String email, String idcardNumber, String plan){
             Customer s;
 
             EntityManagerFactory emf = Persistence.createEntityManagerFactory("$dist/db/Customer.odb");		
@@ -67,12 +81,12 @@ public class Customer {
             System.out.println(q3.getSingleResult());
             int maxPrimaryKey = (int)q3.getSingleResult();
                System.out.println(maxPrimaryKey);
-            s = new Customer(maxPrimaryKey+1, Name, Tel, Email, IdCardNumber, Plan);
+            s = new Customer(maxPrimaryKey+1, name, tel, email, idcardNumber, plan);
             em.persist(s);
 
 
             em.getTransaction().commit();
-        }
+        } ///
     
     
         
---
+++ PetHotel/src/LinkDB/CustomerPetDB.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LinkDB;
+
+/**
+ *
+ * @author natht
+ */
+public class CustomerPetDB {
+    
+        private int primaryKey;
+        private String pet_ID;
+
+    public CustomerPetDB(int primaryKey, String pet_ID) {
+        this.primaryKey = primaryKey;
+        this.pet_ID = pet_ID;
+    }
+        
+         
+    
+}
